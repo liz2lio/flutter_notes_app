628538f2d706ef963b082e82d3e255c4af739bd6
liz2lio <liz2liomedia@gmail.com>
2025-11-25 12:47:09 +0200

    update main screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'controllers/note_controller.dart';
 import 'screens/note_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   Get.lazyPut<NoteController>(() => NoteController());
