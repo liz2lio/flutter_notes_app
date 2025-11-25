5ae047250b74722420ab15a8260f542d04ab15b6
liz2lio <liz2liomedia@gmail.com>
2025-11-25 19:59:11 +0200

    update 25-11

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'controllers/note_controller.dart';
 import 'screens/note_screen.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+//import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-void main() {
-  Get.lazyPut<NoteController>(() => NoteController());
-  runApp(ProviderScope(child: NotesApp()));
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final snapshot = await _firestore.collection('notes').get();
+  snapshot.docs.forEach((doc) => print('${doc.id}: ${doc.data()}'));
+
+  Get.lazyPut<NoteController>(
+    () => NoteController(),
+  );
+  runApp(
+    GetMaterialApp(
+      title: 'My notes',
+      home: NotesApp(),
+    ),
+  );
 }
 
 class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My notes',
-      home: NoteScreen(),
-    );
+    return NoteScreen();
   }
 }
